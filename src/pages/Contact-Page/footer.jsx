1f73a0de10dd788007c9b0c00df2b25edc6734b4
Nelisiwe <nelimajobe@gmail.com>
2023-08-28 12:19:07 +0200

    contact form validation

---
+++ src/pages/Contact-Page/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'; // You can create a CSS file for styling your footer
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
